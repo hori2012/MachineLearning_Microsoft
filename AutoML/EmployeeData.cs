3a3d2100a3f90580bda932286410b8f1cbb6c288
Cong Hao <conghao.hc@gmail.com>
2023-10-07 21:26:23 +0700

    update

--- AutoML/EmployeeData.cs
+++ AutoML/EmployeeData.cs
@@ -1,11 +1,12 @@
 ﻿internal class EmployeeData
 {
     public string? Education { get; set; }
-    public int JoiningYear { get; set; }
+    public float JoiningYear { get; set; }
     public string? City { get; set; }
-    public int PaymentTier { get; set; }
-    public int Age { get; set; }
+    public float PaymentTier { get; set; }
+    public float Age { get; set; }
     public string? Gender { get; set; }
     public bool EverBenched { get; set; }
-    public int ExperienceInCurrentDomain { get; set; }
+    public float ExperienceInCurrentDomain { get; set; }
+    public bool LeaveOrNot { get; set; }
 }
--- AutoML/Program.cs
+++ AutoML/Program.cs
@@ -5,36 +5,36 @@ using static Microsoft.ML.DataOperationsCatalog;
 
 MLContext mLContext = new MLContext();
 //Infer column information
-ColumnInferenceResults columnInference = mLContext.Auto().InferColumns("C:\\Users\\Cong Hao\\Documents\\Data Mining\\ML.NET\\MachineLearning_Microsoft\\AutoML\\Employee.csv", labelColumnName: "LeaveOrNot", groupColumns: false);
-//Create text loader
-TextLoader textLoader = mLContext.Data.CreateTextLoader(columnInference.TextLoaderOptions);
-//load data into IDateView
-IDataView dataView = textLoader.Load("C:\\Users\\Cong Hao\\Documents\\Data Mining\\ML.NET\\MachineLearning_Microsoft\\AutoML\\Employee.csv");
-//Split the data into training and testing sets
-TrainTestData trainTestData = mLContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+//ColumnInferenceResults columnInference = mLContext.Auto().InferColumns("C:\\Users\\Cong Hao\\Documents\\Data Mining\\ML.NET\\MachineLearning_Microsoft\\AutoML\\Employee.csv", labelColumnName: "LeaveOrNot", groupColumns: false);
+////Create text loader
+//TextLoader textLoader = mLContext.Data.CreateTextLoader(columnInference.TextLoaderOptions);
+////load data into IDateView
+//IDataView dataView = textLoader.Load("C:\\Users\\Cong Hao\\Documents\\Data Mining\\ML.NET\\MachineLearning_Microsoft\\AutoML\\Employee.csv");
+////Split the data into training and testing sets
+//TrainTestData trainTestData = mLContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
 
-//Define the experiment settings
-var experimentSettings = new BinaryExperimentSettings();
-experimentSettings.MaxExperimentTimeInSeconds = 400;
+////Define the experiment settings
+//var experimentSettings = new BinaryExperimentSettings();
+//experimentSettings.MaxExperimentTimeInSeconds = 400;
 
-//Create the experiment
-var experiment = mLContext.Auto().CreateBinaryClassificationExperiment(experimentSettings);
+////Create the experiment
+//var experiment = mLContext.Auto().CreateBinaryClassificationExperiment(experimentSettings);
 
-//Run the experiment on the training data and get the best model
-var result = experiment.Execute(trainTestData.TrainSet, columnInference.ColumnInformation);
+////Run the experiment on the training data and get the best model
+//var result = experiment.Execute(trainTestData.TrainSet, columnInference.ColumnInformation);
 
-//Get the best model
-ITransformer model = result.BestRun.Model;
-var metrics = mLContext.BinaryClassification.Evaluate(model.Transform(trainTestData.TestSet), labelColumnName: "LeaveOrNot");
-Console.WriteLine($"Accuracy: {metrics.Accuracy}");
-Console.WriteLine($"F1 Score: {metrics.F1Score}");
-Console.WriteLine($"Log Loss: {metrics.LogLoss}");
-Console.WriteLine($"Entropy: {metrics.Entropy}");
+////Get the best model
+//ITransformer model = result.BestRun.Model;
+//var metrics = mLContext.BinaryClassification.Evaluate(model.Transform(trainTestData.TestSet), labelColumnName: "LeaveOrNot");
+//Console.WriteLine($"Accuracy: {metrics.Accuracy}");
+//Console.WriteLine($"F1 Score: {metrics.F1Score}");
+//Console.WriteLine($"Log Loss: {metrics.LogLoss}");
+//Console.WriteLine($"Entropy: {metrics.Entropy}");
 
 //mLContext.Model.Save(model, trainTestData.TrainSet.Schema, "modelEmployee.zip");
-////goi model da luu
-//DataViewSchema modelSchema;
-//var modelSave = mLContext.Model.Load("modelEmployee.zip", out modelSchema);
-//var sampleData = new EmployeeData { Education = "Bachelors", JoiningYear = 2016, City = "Bangalore", PaymentTier = 1, Age = 33, Gender = "Female", EverBenched = false, ExperienceInCurrentDomain = 0 };
-//var output = mLContext.Model.CreatePredictionEngine<EmployeeData, PredictEmployee>(modelSave).Predict(sampleData);
-//Console.WriteLine("Result: " + output.LeaveOrNot);
+//goi model da luu
+DataViewSchema modelSchema;
+var modelSave = mLContext.Model.Load("modelEmployee.zip", out modelSchema);
+var sampleData = new EmployeeData { Education = "Masters", JoiningYear = 2018, City = "New Delhi", PaymentTier = 3, Age = 27, Gender = "Male", EverBenched = false, ExperienceInCurrentDomain = 5 };
+var output = mLContext.Model.CreatePredictionEngine<EmployeeData, PredictEmployee>(modelSave).Predict(sampleData);
+Console.WriteLine("Result: " + output.LeaveOrNot);
