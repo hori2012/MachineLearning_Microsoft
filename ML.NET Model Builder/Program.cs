844a50768c1af8903171bde278e7beb2da431ce4
Cong Hao <conghao.hc@gmail.com>
2023-11-23 10:29:00 +0700

    update

--- ML.NET Model Builder/Program.cs
+++ ML.NET Model Builder/Program.cs
@@ -1,16 +1,18 @@
-﻿//Load sample data
-using ML_NET_Model_Builder;
+﻿using ML_NET_Model_Builder;
 
+// Create single instance of sample data from first line of dataset for model input
 var imageBytes = File.ReadAllBytes(@"C:\Users\Cong Hao\Documents\Data Mining\ML.NET\MachineLearning_Microsoft\ML.NET Model Builder\Images\ImageTest\Dogs\202.jpg");
+
 ClassficationImages.ModelInput sampleData = new ClassficationImages.ModelInput()
 {
     ImageSource = imageBytes,
 };
+// Make a single prediction on the sample data and print results.
+var sortedScoresWithLabel = ClassficationImages.PredictAllLabels(sampleData);
+Console.WriteLine($"{"Class",-40}{"Score",-20}");
+Console.WriteLine($"{"-----",-40}{"-----",-20}");
 
-//Load model and predict output
-var result = ClassficationImages.Predict(sampleData);
-
-Console.WriteLine($"ImageSource: {result.ImageSource}");
-Console.WriteLine($"Label: {result.Label}");
-Console.WriteLine($"PredictedLabel: {result.PredictedLabel}");
-Console.WriteLine($"Score: {string.Join(" , ", result.Score)}");
+foreach (var score in sortedScoresWithLabel)
+{
+    Console.WriteLine($"{score.Key,-40}{score.Value,-20}");
+}
