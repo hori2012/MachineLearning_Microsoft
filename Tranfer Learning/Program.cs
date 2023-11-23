c2024a52a91b53f5f165ea19560505cd33156c51
Cong Hao <conghao.hc@gmail.com>
2023-11-23 09:54:05 +0700

    update

--- Tranfer Learning/Program.cs
+++ Tranfer Learning/Program.cs
@@ -28,62 +28,62 @@ foreach (var file in files)
     images.Add(new ImageData { ImagePath = file, Label = label });
 }
 
-//// Create an IDataView object from the image list
-//IDataView imageData = mlContext.Data.LoadFromEnumerable(images);
-//// Shuffle the data
-//IDataView shuffledData = mlContext.Data.ShuffleRows(imageData);
+// Create an IDataView object from the image list
+IDataView imageData = mlContext.Data.LoadFromEnumerable(images);
+// Shuffle the data
+IDataView shuffledData = mlContext.Data.ShuffleRows(imageData);
 
-//// Create a pipeline to preprocess the data: convert labels to numerical values and load images as raw bytes
-//var pipeline = mlContext.Transforms.Conversion.MapValueToKey(
-//    inputColumnName: "Label",
-//    outputColumnName: "LabelAsKey")
-//    .Append(mlContext.Transforms.LoadRawImageBytes(
-//        outputColumnName: "Image",
-//        imageFolder: ".",
-//        inputColumnName: "ImagePath"
-//        )
-//    );
+// Create a pipeline to preprocess the data: convert labels to numerical values and load images as raw bytes
+var pipeline = mlContext.Transforms.Conversion.MapValueToKey(
+    inputColumnName: "Label",
+    outputColumnName: "LabelAsKey")
+    .Append(mlContext.Transforms.LoadRawImageBytes(
+        outputColumnName: "Image",
+        imageFolder: ".",
+        inputColumnName: "ImagePath"
+        )
+    );
 
-//// Preprocess the data
-//var preprocessedData = pipeline.Fit(shuffledData).Transform(shuffledData);
-//// Split the data into training and testing sets
-//var trainTestData = mlContext.Data.TrainTestSplit(preprocessedData, testFraction: 0.2);
+// Preprocess the data
+var preprocessedData = pipeline.Fit(shuffledData).Transform(shuffledData);
+// Split the data into training and testing sets
+var trainTestData = mlContext.Data.TrainTestSplit(preprocessedData, testFraction: 0.2);
 
-//// Continue preprocessing the data on the training set
-//var transformedData = pipeline.Fit(trainTestData.TrainSet).Transform(trainTestData.TrainSet);
+// Continue preprocessing the data on the training set
+var transformedData = pipeline.Fit(trainTestData.TrainSet).Transform(trainTestData.TrainSet);
 
-//// Set the options for the image classification model
-//var options = new ImageClassificationTrainer.Options()
-//{
-//    FeatureColumnName = "Image",
-//    LabelColumnName = "LabelAsKey",
-//    ValidationSet = trainTestData.TestSet,
-//    Arch = ImageClassificationTrainer.Architecture.ResnetV250,
-//    MetricsCallback = (metrics) => Console.WriteLine(metrics),
-//    TestOnTrainSet = false,
-//    ReuseTrainSetBottleneckCachedValues = true,
-//    ReuseValidationSetBottleneckCachedValues = true
-//};
+// Set the options for the image classification model
+var options = new ImageClassificationTrainer.Options()
+{
+    FeatureColumnName = "Image",
+    LabelColumnName = "LabelAsKey",
+    ValidationSet = trainTestData.TestSet,
+    Arch = ImageClassificationTrainer.Architecture.ResnetV250,
+    MetricsCallback = (metrics) => Console.WriteLine(metrics),
+    TestOnTrainSet = false,
+    ReuseTrainSetBottleneckCachedValues = true,
+    ReuseValidationSetBottleneckCachedValues = true
+};
 
-//// Add the model training step to the pipeline
-//var trainingPipeline = pipeline.Append(mlContext.MulticlassClassification.Trainers.ImageClassification(options))
-//    .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+// Add the model training step to the pipeline
+var trainingPipeline = pipeline.Append(mlContext.MulticlassClassification.Trainers.ImageClassification(options))
+    .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
 
-//// Train the model
-//var trainedModel = trainingPipeline.Fit(trainTestData.TrainSet);
+// Train the model
+var trainedModel = trainingPipeline.Fit(trainTestData.TrainSet);
 
-//// Predict on the test set
-//var predictions = trainedModel.Transform(trainTestData.TestSet);
+// Predict on the test set
+var predictions = trainedModel.Transform(trainTestData.TestSet);
 
-//// Evaluate the model
-//var metrics = mlContext.MulticlassClassification.Evaluate(predictions, labelColumnName: "LabelAsKey", predictedLabelColumnName: "PredictedLabel");
+// Evaluate the model
+var metrics = mlContext.MulticlassClassification.Evaluate(predictions, labelColumnName: "LabelAsKey", predictedLabelColumnName: "PredictedLabel");
 
-//// Print out the evaluation metrics
-//Console.WriteLine($"LogLoss is: {metrics.LogLoss}");
-//Console.WriteLine($"PerClassLogLoss is: {metrics.PerClassLogLoss}");
+// Print out the evaluation metrics
+Console.WriteLine($"LogLoss is: {metrics.LogLoss}");
+Console.WriteLine($"PerClassLogLoss is: {metrics.PerClassLogLoss}");
 
-//// Save the model to a file
-//mlContext.Model.Save(trainedModel, trainTestData.TrainSet.Schema, "model.zip");
+// Save the model to a file
+mlContext.Model.Save(trainedModel, trainTestData.TrainSet.Schema, "model.zip");
 
 // Load the model from the file
 ITransformer loadedModel = mlContext.Model.Load("model.zip", out var modelSchema);
